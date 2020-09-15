ca7edbeeee2768244b7d72d1720f68f1936e988a
MikaelJCarlsson <60873060+MikaelJCarlsson@users.noreply.github.com>
2020-09-15 10:36:39 +0200

    First collection project

---
+++ Collections/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var saga = new List<string>();
+            saga.Add("Det");
+            saga.Add("var");
+            saga.Add("en");
+            saga.Add("gång");
+            saga.Add("en");
+            saga.Add("saga");
+            foreach (var s in saga)
+            {
+                Console.Write($"{s} ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
